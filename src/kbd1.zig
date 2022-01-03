9587ed1313a8be91df28ed473976c6fbeb07122d
Layne Gustafson <0@laynegustafson.com>
2022-01-02 20:34:27 -0500

    Cleaner blinky

--- src/kbd1.zig
+++ src/kbd1.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 const startup = @import("startup.zig");
 const mcu = @import("peripherals.zig");
 
@@ -7,58 +9,113 @@ comptime {
 }
 
 pub fn main() noreturn {
-    // The stm32l052 starts at 2MHz.
-    // We want at least 16, which we can get from the HSI oscillator.
+    rcc.hsi.enableAndWaitUntilReady();
+    rcc.sys.setSourceAndWaitUntilReady(.hsi);
 
-    // Set HSI16ON.
-    mcu.rcc.cr |= 0b1;
+    rcc.gpio.enablePortC();
 
-    // Wait for HSI16RDYF.
-    while (mcu.rcc.cr & 0b100 == 0) {}
-
-    // Set system clock source to HSI.
-    mcu.rcc.cfgr = (mcu.rcc.cfgr & 0xFF_FF_FF_FC) | 0x00_00_00_01;
-
-    // Wait for HSI to become clock source.
-    while ((mcu.rcc.cfgr & 0x00_00_00_0C) != 0x00_00_00_04) {}
-
-    // Enable GPIO for the LED port (c).
-    mcu.rcc.iopenr |= 0b100;
-
-    // Set the led pin (15) to output.
-    mcu.gpio_c.moder = (mcu.gpio_c.moder & 0x3F_FF_FF_FF) | 0x40_00_00_00;
+    gpio.setMode(.c, 15, .output);
 
     while (true) {
-        // Set LED on.
-        mcu.gpio_c.bsrr |= 0x00_00_80_00;
-
+        gpio.set(.c, 15);
         delay(16_000_000, 500_000);
-
-        // Set LED off.
-        mcu.gpio_c.bsrr |= 0x80_00_00_00;
-
+        gpio.clear(.c, 15);
         delay(16_000_000, 500_000);
     }
-    
-   // stm32.rcc.hsi.enable();
-
-    // while (!stm32.rcc.hsi.ready()) {}
-
-    // stm32.rcc.system_clock.setSource(.hsi16);
-    // while (stm32.rcc.system_clock.getSource() != .hsi16) {}
-
-    // stm32.rcc.apb.setApb1Prescaler(.none);
-    // stm32.rcc.apb.setApb2Prescaler(.none);
-
-    // stm32.rcc.ahb.setPrescaler(.none);
-    // while (stm32.rcc.ahb.getPrescaler() != .none) {}
-
-    // stm32.rcc.gpio.enableClock(led_port);
-
-    // stm32.gpio.setMode(led_port, led_pin, .output);
-
-    // while(true) blink();
 }
+
+const rcc = struct {
+    pub const hsi = struct {
+        pub fn enable() void {
+            setBits(&mcu.rcc.cr, 0, 0, 1);
+        }
+
+        pub fn ready() bool {
+            return getBits(mcu.rcc.cr, 2, 2) == 1;
+        }
+
+        pub fn enableAndWaitUntilReady() void {
+            enable();
+            while (!ready()) {}
+        }
+    };
+
+    pub const sys = struct {
+        pub const Source = enum {
+            msi,
+            hsi,
+            hse,
+            pll,
+        };
+
+        pub fn setSource(source: Source) void {
+            switch (source) {
+                .msi => setBits(&mcu.rcc.cfgr, 0, 1, 0b00),
+                .hsi => setBits(&mcu.rcc.cfgr, 0, 1, 0b01),
+                .hse => setBits(&mcu.rcc.cfgr, 0, 1, 0b10),
+                .pll => setBits(&mcu.rcc.cfgr, 0, 1, 0b11),
+            }
+        }
+
+        pub fn getSource() Source {
+            return switch (getBits(mcu.rcc.cfgr, 2, 3)) {
+                0b00 => .msi,
+                0b01 => .hsi,
+                0b10 => .hse,
+                0b11 => .pll,
+            };
+        }
+
+        pub fn setSourceAndWaitUntilReady(source: Source) void {
+            setSource(source);
+            while (getSource() != source) {}
+        }
+    };
+
+    const gpio = struct {
+        pub fn enablePortC() void {
+            setBits(&mcu.rcc.iopenr, 2, 2, 1);
+        }
+    };
+};
+
+const gpio = struct {
+    pub const Mode = enum {
+        input,
+        output,
+    };
+    
+    pub const Port = enum {
+        c,
+    };
+
+    pub fn setMode(port: Port, pin: u4, mode: Mode) void {
+        const moder = switch (port) {
+            .c => &mcu.gpio_c.moder,
+        };
+
+        setBitsRuntime(moder, @intCast(u5, pin) * 2, @intCast(u5, pin) * 2 + 1, switch (mode) {
+            .input => 0b00,
+            .output => 0b01,
+        });
+    }
+
+    pub fn set(port: Port, pin: u4) void {
+        const bsrr = switch (port) {
+            .c => &mcu.gpio_c.bsrr,
+        };
+
+        setBitsRuntime(bsrr, pin, pin, 1);
+    }
+
+    pub fn clear(port: Port, pin: u4) void {
+        const bsrr = switch (port) {
+            .c => &mcu.gpio_c.bsrr,
+        };
+
+        setBitsRuntime(bsrr, @intCast(u5, pin) + 16, @intCast(u5, pin) + 16, 1);
+    }
+};
 
 fn delay(comptime f_cpu_hz: comptime_int, comptime delay_us: comptime_int) void {
     // On an m0+, nop takes 1 cycle, and then b.n can take 1 or 2 (the documentation link was broken).
@@ -73,4 +130,55 @@ fn delay(comptime f_cpu_hz: comptime_int, comptime delay_us: comptime_int) void 
             "nop"
         );
     }
+}
+
+pub fn InclusiveBitsType(comptime start: u5, comptime end: u5) type {
+    return std.meta.Int(.unsigned, end-start+1);
+}
+
+// Inclusive on both sides (since that's how the datasheet does it).
+pub fn setBits(
+    reg: *align(1) volatile u32,
+    comptime start: u5,
+    comptime end: u5,
+    value: InclusiveBitsType(start, end),
+) void {
+    const mask = ~@as(InclusiveBitsType(start, end), 0);
+    reg.* &= ~(@as(u32, mask) << start);
+    reg.* |= (@as(u32, value) << start);
+}
+
+// Inclusive on both sides.
+pub fn getBits(
+    reg_value: u32,
+    comptime start: u5,
+    comptime end: u5,
+) InclusiveBitsType(start, end) {
+    const mask = ~@as(InclusiveBitsType(start, end), 0);
+    return @intCast(
+        InclusiveBitsType(start, end),
+        (reg_value & (@as(u32, mask) << start)) >> start,
+    );
+}
+
+pub fn setBitsRuntime(
+    reg: *align(1) volatile u32,
+    start: u5,
+    end: u5,
+    value: u32,
+) void {
+    const mask_bits = end - start + 1;
+    const mask = (@as(u32, 1) << mask_bits) - 1;
+    reg.* &= ~(mask << start);
+    reg.* |= (value << start);
+}
+
+pub fn getBitsRuntime(
+    reg_value: u32,
+    start: u5,
+    end: u5,
+) u32 {
+    const mask_bits = end - start + 1;
+    const mask = (@as(u32, 1) << mask_bits) - 1;
+    return (reg_value & (mask << start)) >> start;
 }
